5e20cc5265bd621390391935665e424788fc9a24
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-26 17:00:46 +0530

    Add: last name validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -2,9 +2,11 @@
 echo "Welcome to User Registration Problem";
 
 read -p "Enter First Name:" firstName;
+read -p "Enter Last Name:" lastName;
 
-#Pattern for first name
+#Pattern
 firstcheck="^[A-Z]{1}[a-zA-Z]{2,}$";
+lastcheck="^[A-Z]{1}[a-z A-Z]{2,}$";
 
 #Validation check
 if [[ $firstName =~ $firstcheck ]];
@@ -12,4 +14,11 @@ then
 	echo "valid first name";
 else
 	echo "invalid first name";
+fi
+
+if [[ $lastName =~ $lastcheck ]];
+then
+	echo "valid last name";
+else
+	echo "invalid last name";
 fi
