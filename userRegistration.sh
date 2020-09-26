24893d1f5ddcb1657c2d5e04079775e4cd22e55d
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-26 17:18:33 +0530

    Add: password validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -5,12 +5,14 @@ read -p "Enter First Name:" firstName;
 read -p "Enter Last Name:" lastName;
 read -p "Enter E-mail:" email;
 read -p "Enter Mobile no:" mobileNumber;
+read -p "Enter Password:" password;
 
 #Pattern
 firstcheck="^[A-Z]{1}[a-zA-Z]{2,}$";
 lastcheck="^[A-Z]{1}[a-z A-Z]{2,}$";
 emailcheck="^[0-9a-zA-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-z]+.[a-zA-Z]{2,4}([.][a-zA-z]{2,3})*$"
 mobilecheck="^([9]{1}[1]{1}) [0-9]{10}$";
+passwdcheck="^[0-9a-zA-z]{8,}$";
 
 #Validation check
 if [[ $firstName =~ $firstcheck ]];
@@ -39,4 +41,11 @@ then
 	echo "valid mobile number";
 else
 	echo "invalid mobile number";
+fi
+
+if [[ $password =~ $passwdcheck ]];
+then
+	echo "valid password";
+else
+	echo "invalid password";
 fi
