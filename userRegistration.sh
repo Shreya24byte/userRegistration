a7e10ea9f68012aab420639f30b1d8e7760086d5
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-26 17:07:29 +0530

    Add: email validation

--- userRegistration.sh
+++ userRegistration.sh
@@ -3,10 +3,12 @@ echo "Welcome to User Registration Problem";
 
 read -p "Enter First Name:" firstName;
 read -p "Enter Last Name:" lastName;
+read -p "Enter E-mail:" email;
 
 #Pattern
 firstcheck="^[A-Z]{1}[a-zA-Z]{2,}$";
 lastcheck="^[A-Z]{1}[a-z A-Z]{2,}$";
+emailcheck="^[0-9a-zA-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-z]+.[a-zA-Z]{2,4}([.][a-zA-z]{2,3})*$"
 
 #Validation check
 if [[ $firstName =~ $firstcheck ]];
@@ -21,4 +23,11 @@ then
 	echo "valid last name";
 else
 	echo "invalid last name";
+fi
+
+if [[ $email =~ $emailcheck ]];
+then
+	echo "valid email";
+else
+	echo "invalid email";
 fi
