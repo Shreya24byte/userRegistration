a5329662ab448feab52f895134686c07b7381801
Shreya24byte <shreyanavelkar24@gmail.com>
2020-09-26 22:03:11 +0530

    Modified: password validation should have atleast one number

--- userRegistration.sh
+++ userRegistration.sh
@@ -12,7 +12,7 @@ firstcheck="^[A-Z]{1}[a-zA-Z]{2,}$";
 lastcheck="^[A-Z]{1}[a-z A-Z]{2,}$";
 emailcheck="^[0-9a-zA-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-z]+.[a-zA-Z]{2,4}([.][a-zA-z]{2,3})*$"
 mobilecheck="^([9]{1}[1]{1}) [0-9]{10}$";
-passwdcheck="^[A-Z]+[a-z0-9]{8,}$";
+passwdcheck="^[A-Z]+[0-9]+[a-z]*.{8,}$";
 
 #Validation check
 if [[ $firstName =~ $firstcheck ]];
